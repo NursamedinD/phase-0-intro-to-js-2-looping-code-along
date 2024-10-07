1587b82a1b4f10e431cc2b78b277eb84109dae2e
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2024-10-07 22:23:29 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    var messages = [];
+    
+    for (var i = 0; i < names.length; i++) {
+        var message = "Thank you, " + names[i] + ", for the wonderful " + event + " gift!";
+        messages.push(message);
+    }
+
+    return messages;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num = num - 1;
+    }
+}
